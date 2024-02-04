13c2af1eb95fb22c71afc0b74645095c87bfe329
Xavier Desoindre <xavier.desoindre@hotmail.fr>
2024-02-04 11:39:08 +0100

    fix: preact

--- next.config.js
+++ next.config.js
@@ -2,11 +2,19 @@ const withNextIntl = require("next-intl/plugin")("./src/i18n.ts");
 
 /** @type {import('next').NextConfig} */
 const nextConfig = withNextIntl({
-  resolve: {
-    alias: {
-      react: "preact-compat",
-      "react-dom": "preact-compat",
-    },
+  reactStrictMode: true,
+  swcMinify: true,
+
+  webpack: (config, { dev, isServer }) => {
+    if (!dev && !isServer) {
+      Object.assign(config.resolve.alias, {
+        "react/jsx-runtime.js": "preact/compat/jsx-runtime",
+        react: "preact/compat",
+        "react-dom/test-utils": "preact/test-utils",
+        "react-dom": "preact/compat",
+      });
+    }
+    return config;
   },
 });
 
