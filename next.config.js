c2716d17ac65d9e287bd31b86f5025d74140a8c7
Xavier Desoindre <xavier.desoindre@hotmail.fr>
2024-02-04 11:33:16 +0100

    add preact

--- next.config.js
+++ next.config.js
@@ -1,6 +1,13 @@
 const withNextIntl = require("next-intl/plugin")("./src/i18n.ts");
 
 /** @type {import('next').NextConfig} */
-const nextConfig = withNextIntl({});
+const nextConfig = withNextIntl({
+  resolve: {
+    alias: {
+      react: "preact-compat",
+      "react-dom": "preact-compat",
+    },
+  },
+});
 
 module.exports = nextConfig;
