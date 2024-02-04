ab530a0b7867f0548731e079d3c147c20faad2be
Xavier Desoindre <xavier.desoindre@hotmail.fr>
2024-02-04 11:55:34 +0100

    renable react

--- next.config.js
+++ next.config.js
@@ -4,18 +4,6 @@ const withNextIntl = require("next-intl/plugin")("./src/i18n.ts");
 const nextConfig = withNextIntl({
   reactStrictMode: true,
   swcMinify: true,
-
-  webpack: (config, { dev, isServer }) => {
-    if (!dev && !isServer) {
-      Object.assign(config.resolve.alias, {
-        "react/jsx-runtime.js": "preact/compat/jsx-runtime",
-        react: "preact/compat",
-        "react-dom/test-utils": "preact/test-utils",
-        "react-dom": "preact/compat",
-      });
-    }
-    return config;
-  },
 });
 
 module.exports = nextConfig;
--- src/components/Header/Flags.tsx
+++ src/components/Header/Flags.tsx
@@ -3,7 +3,7 @@
 import React from "react";
 import NextLink from "next/link";
 import Image from "next/image";
-import { usePathname, useRouter } from "next/navigation";
+import { usePathname } from "next/navigation";
 
 const Flags = () => {
   const path = usePathname();
